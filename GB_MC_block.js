cf722efbab3bf32325cdcb10eaa7891df6dea4b0
FSFatScooter <FSFatScooter@gmail.com>
2011-11-24 23:27:41 -0600

    Added a function to test how blocks look rendered in a grid. Tree is visible.

--- GB_MC_block.js
+++ GB_MC_block.js
@@ -178,16 +178,94 @@ function Leaves(x,y){
 	Rect(x+7,y+1);
 	Rect(x+11,y+1);
 	
-	Rect(x,y);
+	Rect(x+4,y+2);
+	Rect(x+6,y+2);
+	Rect(x+8,y+2);
+	Rect(x+10,y+2);
+	
+	Rect(x,y+3,2);
+	Rect(x+3,y+3);
+	Rect(x+5,y+3);
+	Rect(x+7,y+3);
+	Rect(x+9,y+3);
+	Rect(x+11,y+3);
+	
+	Rect(x+1,y+4);
+	Rect(x+3,y+4,2);
+	Rect(x+7,y+4,2);
+	Rect(x+10,y+4);
+	
+	Rect(x,y+5);
+	Rect(x+2,y+5);
+	Rect(x+5,y+5,2);
+	Rect(x+9,y+5,1,2);
+	
+	Rect(x+3,y+6,2);
+	Rect(x+7,y+6);
+	Rect(x+11,y+6);
+	
+	Rect(x,y+7);
+	Rect(x+2,y+7);
+	Rect(x+4,y+7);
+	Rect(x+6,y+7);
+	Rect(x+8,y+7);
+	
+	Rect(x+1,y+8);
+	Rect(x+3,y+8);
+	Rect(x+5,y+8);
+	Rect(x+7,y+8,1,2);
+	Rect(x+9,y+8);
+	Rect(x+11,y+8);
+	
+	Rect(x,y+9);
+	Rect(x+4,y+9);
+	Rect(x+10,y+9);
+	
+	Rect(x+1,y+10);
+	Rect(x+3,y+10);
+	Rect(x+5,y+10,2);
+	Rect(x+8,y+10);
+	Rect(x+11,y+10,1,2);
+	
+	Rect(x,y+11);
+	Rect(x+2,y+11);
+	Rect(x+4,y+11);
+	Rect(x+7,y+11);
+	Rect(x+9,y+11);
+}
+
+function Planks(x,y){
+	x=Real(x);
+	y=Real(y);
+	
+	Fill(gd);
+	Rect(x,y,12,12);
+	
+	Fill(gl);
+	Rect(x,y,12);
+	Rect(x,y+4,12);
+	Rect(x,y+8,12);
+	
+	Rect(x+7,y+1,2);
+	Rect(x+2,y+2);
+	Rect(x+5,y+2);
+	Rect(x+10,y+3);
+	Rect(x,y+5);
+	Rect(x+9,y+6);
+	Rect(x+4,y+7,2);
+	Rect(x+2,y+10);
+	Rect(x+7,y+10);
+	Rect(x+4,y+11);
+	Rect(x+9,y+11,2);
 }
 
 function Stone(x,y){
 	x=Real(x);
 	y=Real(y);
 	
-	Fill(gl);
-	Rect(x,y,12,12);
 	Fill(gd);
+	Rect(x,y,12,12);
+	Fill(bl);
 	
 	Rect(x,y);
 	Rect(x+2,y)
--- GB_MC_terrain.js
+++ GB_MC_terrain.js
@@ -4,7 +4,40 @@
 
 
 function fakeMap(){
-	Stone(12,132);
-	Stone(12,120);
-	Leaves();
+//	Stone(12,132);
+//	Stone(12,120);
+	Leaves(12);
+	Leaves(24);
+	Leaves(36);
+	
+	Leaves(0,12);
+	Leaves(12,12);
+	Leaves(24,12);
+	Leaves(36,12);
+	Leaves(48,12);
+	
+	Leaves(0,24);
+	Leaves(12,24);
+	Leaves(24,24);
+	Leaves(36,24);
+	Leaves(48,24);
+	
+	Leaves(12,36);
+	Wood(24,36);
+	Leaves(36,36);
+	
+	Wood(24,48);
+	Wood(24,60);
+	Wood(24,72);
+	
+	Planks(72);
+	Planks(84);
+	Planks(96);
+	Planks(72,12);
+	Planks(84,12);
+	Planks(96,12);
+	Planks(72,24);
+	Planks(84,24);
+	Planks(96,24);
+	
 }
